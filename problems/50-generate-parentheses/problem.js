070cd0c5238125760cea9407ed22fb1483477cbc
hassam7 <hassam.ali@gmail.com>
2026-07-21 19:45:31 +0500

    generate paran

--- problems/50-generate-parentheses/problem.js
+++ problems/50-generate-parentheses/problem.js
@@ -6,36 +6,5 @@
 // The order of combinations does not matter.
 // generateParentheses(3); // ['((()))', '(()())', '(())()', '()(())', '()()()']
 var generateParentheses = function (n) {
-  if (typeof n === 'number' && n === 0) {
-    return [''];
-  }
 
-  if (typeof n === 'number') {
-    var allOptions = generateParentheses({ str: '', length: n * 2 });
-
-    return allOptions.filter(option => {
-      var balance = 0;
-
-      for (var i = 0; i < option.length; i++) {
-        if (option[i] === '(') {
-          balance++;
-        } else {
-          balance--;
-        }
-
-        if (balance < 0) {
-          return false;
-        }
-      }
-
-      return balance === 0;
-    });
-  }
-
-  if (n.str.length === n.length) {
-    return [n.str];
-  }
-
-  return generateParentheses({ str: n.str + '(', length: n.length })
-    .concat(generateParentheses({ str: n.str + ')', length: n.length }));
 };
